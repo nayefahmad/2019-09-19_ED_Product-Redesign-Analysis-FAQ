3788f27226f0cadec70e866c209d8c5a30f6f0e8
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-06 13:34:36 -0800

    add holidays df + update data source

--- src/2019-10-29_rhs_ED-census-modeling.R
+++ src/2019-10-29_rhs_ED-census-modeling.R
@@ -29,7 +29,7 @@ setup_denodo()
 cnx <- DBI::dbConnect(odbc::odbc(), dsn = "cnx_SPDBSCSTA001")
 
 vw_ed_census <- dplyr::tbl(cnx, dbplyr::in_schema("DSSI.dbo", 
-                                                  "[AISAKE_hourly_daily_EDCensus_2]"))
+                                                  "[AISAKE_Hourly_Census]"))
 
 #+ rest
 
@@ -59,12 +59,13 @@ df1.census_raw <-
   filter(FacilityShortName == site, 
          short_dt >= start_date_id_param) %>% 
   group_by(short_dt,
-           ShortDate,
+           # ShortDate,
            TOD,
            DOW, 
            CalendarMonth, 
            CalendarYear, 
-           Effective_Holiday_Flag) %>% 
+           HolidayName
+           ) %>% 
   summarise(ED_Census = sum(ED_Census, na.rm = TRUE)) %>% 
   collect() %>% 
   ungroup() %>% 
@@ -91,16 +92,17 @@ df2.census <-
                                                             "Sunday")), 
          calendar_month = calendar_month %>% factor(levels = month.name), 
          calendar_year = as.factor(calendar_year), 
-         effective_holiday_flag = ifelse(is.na(effective_holiday_flag), 
-                                               0, 
-                                               effective_holiday_flag) %>% as.factor) %>% 
+         holiday_name = if_else(is.na(holiday_name),
+                                "not_holiday", 
+                                holiday_name)) %>% 
+  mutate(holiday_name = holiday_name %>% as.factor()) %>% 
   select(short_dt, 
          date, 
          hour, 
          weekday, 
          month = calendar_month,  # rename within select( )
          year = calendar_year, 
-         hol = effective_holiday_flag, 
+         hol = holiday_name, 
          ed_census)
   
 
@@ -123,11 +125,10 @@ df2.census %>%
 
 df3.hols <- 
   df2.census %>% 
-  filter(hol == 1) %>% 
-  mutate(holiday = NA, 
-         lower_window = 0, 
+  filter(hol != "not_holiday") %>% 
+  mutate(lower_window = 0, 
          upper_window = 0) %>% # include 0 days before and after the hol
-  select(holiday, 
+  select(holiday = hol, 
          ds = date, 
          lower_window, 
          upper_window) %>% # %>% View
@@ -138,10 +139,10 @@ df3.hols <-
 
 
 
-# df3.hols %>% 
-#   datatable(extensions = 'Buttons',
-#             options = list(dom = 'Bfrtip', 
-#                            buttons = c('excel', "csv")))
+df3.hols %>%
+  datatable(extensions = 'Buttons',
+            options = list(dom = 'Bfrtip',
+                           buttons = c('excel', "csv")))
                            
 
 
@@ -199,6 +200,8 @@ p <- df2.census %>%
              y = ed_census, 
              label = date)) + 
   geom_boxplot() + 
+  theme(axis.text.x = element_text(angle = 45, 
+                                   hjust = 1)) + 
   facet_wrap(~year); ggplotly(p)
 
 p <- df2.census %>% 
@@ -208,7 +211,10 @@ p <- df2.census %>%
              label = date)) + 
   geom_boxplot() + 
   labs(title = "Hours from 7am to 2pm") + 
-  facet_wrap(~year); ggplotly(p)
+  
+  facet_wrap(~year) + 
+  theme(axis.text.x = element_text(angle = 45, 
+                                   hjust = 1)); ggplotly(p)
 
 
 
@@ -220,18 +226,18 @@ p <- df2.census %>%
 #' 
 # Models ----------
 # > Train/test split -------------
-train_start <- df2.census$date %>% min
+t1.train_start <- df2.census$date %>% min
 
-test_end <- df2.census$date %>% max
-test_start <- test_end - 30 
+t4.test_end <- df2.census$date %>% max
+t3.test_start <- t4.test_end - 30 
 
-train_end <- test_start - 1 
+t2.train_end <- t3.test_start - 1 
 
 horizon_param <- 24*7*2  # unit: hours 
 
-#' **We take train data from `r train_start` to `r train_end`.** 
+#' **We take train data from `r t1.train_start` to `r t2.train_end`.** 
 #' 
-#' **Test data is from `r test_start` to `r test_end`**
+#' **Test data is from `r t3.test_start` to `r t4.test_end`**
 #' 
 
 
@@ -239,25 +245,26 @@ df4.train <-
   df2.census %>%
   select(ds = short_dt,
          y = ed_census) %>% 
-  filter(ds <= train_end)
+  filter(ds <= t2.train_end)
 
 df5.test <- 
   df2.census %>%
   select(ds = short_dt,
          y = ed_census) %>% 
-  filter(ds >= test_start) %>% 
+  filter(ds >= t3.test_start) %>% 
   mutate(date = date(ds), 
          hour = hour(ds)) %>% 
   arrange(ds)
 
 
-
+#+ model 1
 #' ## Prophet model 1 
 #' 
 
 # > Prophet model 1  ---------
 # fit prophet model:  
 m1 <- prophet(df4.train,
+              holidays = df3.hols, 
               changepoint.prior.scale = 0.05) # default is 0.05; Increasing it will make the trend more flexible:
 
 # future df: 
@@ -275,6 +282,26 @@ plot(m1, fcast) +
 
 prophet_plot_components(m1, fcast)
 prophet:::plot_yearly(m1)
+
+# look at fitted values and actuals over last 14 days 
+fcast %>% 
+  filter(ds > t2.train_end - 14) %>% 
+  inner_join(df4.train) %>% 
+  
+  ggplot(aes(x = ds, y = yhat)) + 
+  geom_ribbon(aes(ymin = yhat_lower, 
+                  ymax = yhat_upper), 
+              col = "grey", alpha = 0.3) + 
+  # fitted value: 
+  geom_line(aes(y = trend + additive_terms), 
+            col = "black") + 
+  # actuals: 
+  geom_line(aes(y = y), 
+            col = "blue") + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"))
+  
 
 
 # fcast df: 
@@ -297,15 +324,20 @@ df7.1_metrics_m1 %>%
             options = list(dom = 'Bfrtip', 
                            buttons = c('excel', "csv")))
 
-#' **Prophet's default is to return 80% prediction intervals.** 
+#' **Prophet's default is to return 80% uncertainty intervals.** 
+#' 
+#' See: https://facebook.github.io/prophet/docs/uncertainty_intervals.html 
+#' 
+#' I'm not sure if "uncertainty" interval maps exactly onto "prediction interval"
 #' 
 
-
+#+ model 2
 #' ## Prophet model 2
 #' 
 # > Prophet model 2  ---------
 # fit prophet model:  
 m2 <- prophet(df4.train,
+              holidays = df3.hols,
               changepoint.prior.scale = 0.01) # default is 0.05; Increasing it will make the trend more flexible:
 
 # future df: 
@@ -323,6 +355,25 @@ plot(m2, fcast) +
 
 prophet_plot_components(m2, fcast)
 prophet:::plot_yearly(m2)
+
+# look at fitted values and actuals over last 14 days 
+fcast %>% 
+  filter(ds > t2.train_end - 14) %>% 
+  inner_join(df4.train) %>% 
+  
+  ggplot(aes(x = ds, y = yhat)) + 
+  geom_ribbon(aes(ymin = yhat_lower, 
+                  ymax = yhat_upper), 
+              col = "grey", alpha = 0.3) + 
+  # fitted value: 
+  geom_line(aes(y = trend + additive_terms), 
+            col = "black") + 
+  # actuals: 
+  geom_line(aes(y = y), 
+            col = "blue") + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"))
 
 
 # fcast df: 
@@ -346,12 +397,13 @@ df7.2_metrics_m2 %>%
                            buttons = c('excel', "csv")))
 
 
-
+#+ model 3
 #' ## Prophet model 3
 #' 
 # > Prophet model 3  ---------
 # fit prophet model:  
 m3 <- prophet(df4.train,
+              holidays = df3.hols,
               changepoint.prior.scale = 0.05, # default is 0.05; Increasing it will make the trend more flexible:
               changepoint.range = 0.90, 
               yearly.seasonality = 5)  # default is 10; add Fourier terms for more flexibility in fitting yearly seasonality  
@@ -371,6 +423,25 @@ plot(m3, fcast) +
 
 prophet_plot_components(m3, fcast)
 prophet:::plot_yearly(m3)
+
+# look at fitted values and actuals over last 14 days 
+fcast %>% 
+  filter(ds > t2.train_end - 14) %>% 
+  inner_join(df4.train) %>% 
+  
+  ggplot(aes(x = ds, y = yhat)) + 
+  geom_ribbon(aes(ymin = yhat_lower, 
+                  ymax = yhat_upper), 
+              col = "grey", alpha = 0.3) + 
+  # fitted value: 
+  geom_line(aes(y = trend + additive_terms), 
+            col = "black") + 
+  # actuals: 
+  geom_line(aes(y = y), 
+            col = "blue") + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"))
 
 
 # fcast df: 
@@ -402,15 +473,16 @@ df7.3_metrics_m3 %>%
 
 
 
-
-
-
-
-
-                           
+#+ model-select
 #' ## Model selection 
+#' 
 # Model selection -----
 
+#' Based on cross-validated RMSE: 
+#' 
+#' * Model 1 is clearly better than Model 2
+#' 
+#' 
 
 
 #' ## Test set accuracy
@@ -427,7 +499,7 @@ df7.3_metrics_m3 %>%
 #          yhat_upper) %>% 
 #   mutate(date = date(ds), 
 #          hour = hour(ds)) %>% 
-#   filter(date >= min_date + train_end & hour > 0) %>% 
+#   filter(date >= min_date + t2.train_end & hour > 0) %>% 
 #   
 #   left_join(df5.test, 
 #             by = c("date" = "date", 
