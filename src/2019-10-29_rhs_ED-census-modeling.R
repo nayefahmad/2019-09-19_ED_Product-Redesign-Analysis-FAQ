72c24a77771fc7ecdc0ba429cc8c5a97acb36434
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-06 16:06:41 -0800

    test set RMSE + residual plots

--- src/2019-10-29_rhs_ED-census-modeling.R
+++ src/2019-10-29_rhs_ED-census-modeling.R
@@ -24,7 +24,7 @@ library(prophet)
 library(DT)
 library(plotly)
 
-setup_denodo()
+# setup_denodo()
 
 cnx <- DBI::dbConnect(odbc::odbc(), dsn = "cnx_SPDBSCSTA001")
 
@@ -33,24 +33,32 @@ vw_ed_census <- dplyr::tbl(cnx, dbplyr::in_schema("DSSI.dbo",
 
 #+ rest
 
-#' # Todo: 
+#' # Todo:
+#'
+#' 1. test set RMSE
+#'
+#' 2. Residual analysis
+#'
+#' 3. Will the seasonality across the year smoothen out a lot if we increase the
+#' window around the holidays?
 #' 
-#' 1. use DSSI.dbo.AISAKE_Hourly_Census
+
+
+#' # Overview 
 #' 
-#' 2. check NAs in census table 
-#' 
-#' 3. Missing holidays? 
-#' 
+#' **What are we doing and why are we doing it? **
 #' 
 
 #' # Parameters 
 #' 
-#' Prophet hangs with ten years of hourly data. Let's start with 3 years. 
+#' `prophet` hangs with ten years of hourly data. Let's start with 3 years. 
 #' 
 # Parameters -----------
 start_date_id_param <- "20170101"  # todo: experiment with this
 site <- "RHS"
 
+
+#+ data
 #' # Data
 #' 
 # Data -----------
@@ -59,7 +67,6 @@ df1.census_raw <-
   filter(FacilityShortName == site, 
          short_dt >= start_date_id_param) %>% 
   group_by(short_dt,
-           # ShortDate,
            TOD,
            DOW, 
            CalendarMonth, 
@@ -221,9 +228,18 @@ p <- df2.census %>%
 
 #+ models 
 #' # Models 
+
+#' ## Parameters and data splitting
+#'
+#' Different models are just different lenses through which we look at our data.
+#' We don't believe they're *true*, but we believe they can *highlight key
+#' features of the system.*
+#'
+#' We're using a forecasting model called from the R package `{prophet}`. 
 #' 
-#' ## Parameters and data splitting  
+#' See here for details: https://facebook.github.io/prophet/ 
 #' 
+
 # Models ----------
 # > Train/test split -------------
 t1.train_start <- df2.census$date %>% min
@@ -274,17 +290,17 @@ future <- make_future_dataframe(m1,
 
 
 # Fcast:  
-fcast <- predict(m1, future)
+fcast1 <- predict(m1, future)
 
 # Examine the fitted model and fcast
-plot(m1, fcast) + 
+plot(m1, fcast1) + 
   add_changepoints_to_plot(m1)
 
-prophet_plot_components(m1, fcast)
+prophet_plot_components(m1, fcast1)
 prophet:::plot_yearly(m1)
 
 # look at fitted values and actuals over last 14 days 
-fcast %>% 
+fcast1 %>% 
   filter(ds > t2.train_end - 14) %>% 
   inner_join(df4.train) %>% 
   
@@ -303,14 +319,17 @@ fcast %>%
         panel.grid.major = element_line(colour = "grey95"))
   
 
+#' Sampe of 100 rows: 
 
 # fcast df: 
-fcast %>%
-  head %>% 
+fcast1 %>%
+  sample_n(100) %>% 
   datatable(extensions = 'Buttons',
             options = list(dom = 'Bfrtip',
                            buttons = c('excel', "csv")))
 
+#' ### Cross-validation
+#' 
 # Cross val: 
 df7.1_metrics_m1 <- 
   cross_validation(m1, 
@@ -347,17 +366,17 @@ future <- make_future_dataframe(m2,
 
 
 # Fcast:  
-fcast <- predict(m2, future)
+fcast2 <- predict(m2, future)
 
 # Examine the fitted model and fcast
-plot(m2, fcast) + 
+plot(m2, fcast2) + 
   add_changepoints_to_plot(m2)
 
-prophet_plot_components(m2, fcast)
+prophet_plot_components(m2, fcast2)
 prophet:::plot_yearly(m2)
 
 # look at fitted values and actuals over last 14 days 
-fcast %>% 
+fcast2 %>% 
   filter(ds > t2.train_end - 14) %>% 
   inner_join(df4.train) %>% 
   
@@ -376,12 +395,14 @@ fcast %>%
         panel.grid.major = element_line(colour = "grey95"))
 
 
-# fcast df: 
-fcast %>%
+# fcast2 df: 
+fcast2 %>%
   head %>% 
   datatable(extensions = 'Buttons',
             options = list(dom = 'Bfrtip',
                            buttons = c('excel', "csv")))
+#' ### Cross-validation
+#' 
 
 # Cross val: 
 df7.2_metrics_m2 <- 
@@ -415,17 +436,17 @@ future <- make_future_dataframe(m3,
 
 
 # Fcast:  
-fcast <- predict(m3, future)
+fcast3 <- predict(m3, future)
 
 # Examine the fitted model and fcast
-plot(m3, fcast) + 
+plot(m3, fcast3) + 
   add_changepoints_to_plot(m3)
 
-prophet_plot_components(m3, fcast)
+prophet_plot_components(m3, fcast3)
 prophet:::plot_yearly(m3)
 
 # look at fitted values and actuals over last 14 days 
-fcast %>% 
+fcast3 %>% 
   filter(ds > t2.train_end - 14) %>% 
   inner_join(df4.train) %>% 
   
@@ -445,11 +466,14 @@ fcast %>%
 
 
 # fcast df: 
-fcast %>%
+fcast3 %>%
   head %>% 
   datatable(extensions = 'Buttons',
             options = list(dom = 'Bfrtip',
                            buttons = c('excel', "csv")))
+
+#' ### Cross-validation
+#' 
 
 # Cross val: 
 df7.3_metrics_m3 <- 
@@ -476,11 +500,14 @@ df7.3_metrics_m3 %>%
 #+ model-select
 #' ## Model selection 
 #' 
-# Model selection -----
-
-#' Based on cross-validated RMSE: 
+#' To perform model selection, we get a cross-validated RMSE for each model for
+#' a 14-day horizon fcast, based on the training data (see above).
+#'
+#' Test data is not used for model selection. It is only used after model
+#' selection to get our best possible estimate of the performance of the
+#' selected model on completely new data.
 #' 
-#' * Model 1 is clearly better than Model 2
+#' **Based on cross-validated RMSE, Model 1 is clearly better than Model 2 and Model 3**
 #' 
 #' 
 
@@ -491,44 +518,160 @@ df7.3_metrics_m3 %>%
 #' cross-validated train set accuracy.
 
 # > Test set accuracy -------------
-# df6.test_accuracy <- 
-#   fcast %>% 
-#   select(ds, 
-#          yhat, 
-#          yhat_lower, 
-#          yhat_upper) %>% 
-#   mutate(date = date(ds), 
-#          hour = hour(ds)) %>% 
-#   filter(date >= min_date + t2.train_end & hour > 0) %>% 
-#   
-#   left_join(df5.test, 
-#             by = c("date" = "date", 
-#                    "hour" = "hour")) %>% 
-#   mutate(error = y - yhat)
-#   
-# str(df6.test_accuracy)
-# summary(df6.test_accuracy$error)
-# 
-# df6.test_accuracy %>% 
-#   filter(date < "2019-04-15") %>%
-#   ggplot(aes(x = ds.x, 
-#              y = yhat)) + 
-#   geom_ribbon(aes(x = ds.x, 
-#                   ymin = yhat_lower,
-#                   ymax = yhat_upper), 
-#               fill = "grey80",
-#               alpha = 0.5) + 
-#   geom_line() + 
-#   geom_point() + 
-#   
-#   geom_line(aes(x = ds.x, 
-#                 y = y), 
-#             col = "skyblue")
+df8.test_accuracy <-
+  fcast1 %>%
+  select(ds,
+         yhat,
+         yhat_lower,
+         yhat_upper) %>%
+  mutate(date = date(ds),
+         hour = hour(ds)) %>%
+  filter(date >= t3.test_start) %>%
+
+  left_join(df5.test,
+            by = c("date" = "date",
+                   "hour" = "hour")) %>%
+  mutate(error = y - yhat)
   
+# str(df8.test_accuracy)
+# summary(df8.test_accuracy)
+# sqrt(mean((df8.test_accuracy$error)^2))
+
+#' **RMSE on test set is `r sqrt(mean((df8.test_accuracy$error)^2))`**
+#' 
+
+# look at fitted values and actuals over fcast horizon:  
+df8.test_accuracy %>%   
+  ggplot(aes(x = ds.x,
+             y = yhat)) + 
+  geom_ribbon(aes(ymin = yhat_lower, 
+                  ymax = yhat_upper), 
+              col = "grey",
+              alpha = 0.3) + 
+  # fitted value: 
+  geom_line(aes(y = yhat), 
+            col = "black") + 
+  # actuals: 
+  geom_line(aes(y = y), 
+            col = "blue") + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"))
+
+
+
+
+
   
 #' # Analyzing residuals from model 
 #' 
-
 # Analyzing residuals --------
 
+df9.train_errors <- 
+  fcast1 %>%
+  select(ds,
+         yhat,
+         yhat_lower,
+         yhat_upper) %>%
+  mutate(date = date(ds),
+         hour = hour(ds) %>% as.factor()) %>%
+  filter(date < t3.test_start) %>%
+  
+  left_join(df2.census,
+            by = c("date" = "date",
+                   "hour" = "hour")) %>%
+  mutate(error = ed_census - yhat) %>% 
+  select(ds, 
+         ed_census, 
+         yhat, 
+         error)
 
+# sample 100: 
+df9.train_errors %>% 
+  sample_n(100) %>% 
+  datatable(extensions = 'Buttons',
+            options = list(dom = 'Bfrtip', 
+                           buttons = c('excel', "csv")))
+                           
+# plot
+df9.train_errors %>% 
+  ggplot(aes(x = error)) + 
+  geom_density() + 
+  geom_vline(xintercept = 0, 
+             col = "blue") + 
+  
+  labs(title = "Training data errors (actual minus fitted)") + 
+  
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"))
+  
+
+df9.train_errors %>% 
+  ggplot(aes(x = ds, 
+             y = error)) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth()
+  
+qqnorm(df9.train_errors$error)  
+qqline(df9.train_errors$error, col = "red")  
+
+# try fitting a normal dist: 
+m4.normal_train_error <- 
+  df9.train_errors$error %>% 
+  fitdistrplus::fitdist("norm")
+
+# summary(m4.normal_train_error)
+# m4.normal_train_error$estimate[1] %>% unname
+# m4.normal_train_error$estimate[2] %>% unname
+
+#' In train data, approx 95% of all fitted values are 
+#' within `r m4.normal_train_error$estimate[2] %>% unname *2` patients of the 
+#' actual census value.
+#' 
+
+#' # Retrain selected model on full data 
+#' 
+# Retrain selected model on full data ----------
+# todo
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' *****************************************************************
+
+#' # Appendix
+#' 
+#' ## Libraries and connections 
+#' 
+# library(tidyverse)
+# library(denodoExtractor)
+# library(janitor)
+# library(lubridate)
+# library(prophet)
+# library(DT)
+# library(plotly)
+# 
+# setup_denodo()
+# 
+# cnx <- DBI::dbConnect(odbc::odbc(), dsn = "cnx_SPDBSCSTA001")
+# 
+# vw_ed_census <- dplyr::tbl(cnx, dbplyr::in_schema("DSSI.dbo", 
+#                                                   "[AISAKE_Hourly_Census]"))
+
+#' ## Outputs 
+#' 
+# write_csv(df2.census,
+#           here::here("results", 
+#                      "dst", 
+#                      "2019-11-06_rhs_ed-census-by-hour-from-2017.csv"))
+             
